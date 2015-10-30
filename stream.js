f9dae269179705028321c7ff7732aa04a12d825c
Josh Eversmann <josh.eversmann@utexas.edu>
2015-10-30 10:34:04 -0500

    Things from the twitter package demos

---
+++ stream.js
@@ -0,0 +1,18 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
