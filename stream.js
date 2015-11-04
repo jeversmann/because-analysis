2ae40b15fe81fa5cf21d6a316bebb1364fcf510d
Josh Eversmann <josh.eversmann@utexas.edu>
2015-11-04 11:25:58 -0600

    Proof of concept from last weekend

--- stream.js
+++ stream.js
@@ -1,13 +1,14 @@
+require('dotenv').load();
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+client.stream('statuses/filter', {track: 'because'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
